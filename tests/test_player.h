03c6498480882ded7a6d5b575f596e59019c95ee
boki916111 <gyww60@durham.ac.uk>
2025-11-12 21:03:21 +0000

    implement the test for function getOrCreatePlayer works correctly

--- tests/test_player.h
+++ tests/test_player.h
@@ -4,5 +4,5 @@
 
 #ifndef ROCK_PAPER_SCISSORS_GAME_TEST_PLAYER_H
 #define ROCK_PAPER_SCISSORS_GAME_TEST_PLAYER_H
-
+void test_get_or_create_player();
 #endif //ROCK_PAPER_SCISSORS_GAME_TEST_PLAYER_H

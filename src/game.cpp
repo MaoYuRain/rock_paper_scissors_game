80f272661d383ac74b4e3ff1cc1340a9415a5447
WZT-1221 <a1203368876@gmail.com>
2025-11-12 17:53:06 +0000

    implement function getMaskedChoice

--- src/game.cpp
+++ src/game.cpp
@@ -2,6 +2,13 @@
 // Created by maoyu on 2025/11/12.
 //
 #include <iostream>
+#ifdef _WIN32
+#include <conio.h>   // Windows
+#else
+#include <termios.h> // macOS / Linux
+#include <unistd.h>
+#endif
+
 int decideWinner(int p1, int p2) {
     // 1 = rock, 2 = paper, 3 = scissors
     if (p1 == p2) return 0;
@@ -13,4 +20,38 @@ int decideWinner(int p1, int p2) {
             return 1;
     else
         return 2;
+}
+int getMaskedChoice(int playerNumber) {
+    while (true) {
+        std::cout << "Player " << playerNumber
+                  << ", enter your choice (1=rock, 2=paper, 3=scissors): ";
+
+#ifdef _WIN32
+        // Windows
+        char ch = _getch();
+#else
+        // macOS / Linux
+        termios oldt, newt;
+        tcgetattr(STDIN_FILENO, &oldt);
+        newt = oldt;
+        newt.c_lflag &= ~(ICANON | ECHO);
+        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+        char ch = getchar();
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+#endif
+
+        std::cout << "*\n";
+
+        if (ch >= '1' && ch <= '3')
+            return ch - '0';
+        else {
+            std::cout << "Invalid input! Please enter 1, 2, or 3.\n";
+
+#ifdef _WIN32
+            while (_kbhit()) _getch(); // 清空缓冲
+#else
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+#endif
+        }
+    }
 }

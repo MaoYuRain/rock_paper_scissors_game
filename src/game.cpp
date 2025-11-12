994578a3d001db3677923648326c5dc4e390bd0f
boki916111 <gyww60@durham.ac.uk>
2025-11-12 18:01:15 +0000

    implement function choiceToString

--- src/game.cpp
+++ src/game.cpp
@@ -14,3 +14,12 @@ int decideWinner(int p1, int p2) {
     else
         return 2;
 }
+// 数字转文字
+std::string choiceToString(int choice) {
+    switch (choice) {
+        case 1: return "Rock";
+        case 2: return "Paper";
+        case 3: return "Scissors";
+        default: return "Invalid";
+    }
+}

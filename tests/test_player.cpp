e207d2104b31b44fbb5d85c9f909eb44539fdb70
boki916111 <gyww60@durham.ac.uk>
2025-11-12 20:58:12 +0000

    implement the test for function choiceToString

--- tests/test_player.cpp
+++ tests/test_player.cpp
@@ -1,10 +1,3 @@
 //
 // Created by maoyu on 2025/11/12.
 //// ---------- choiceToString() 测试 ----------
-TEST_CASE("choiceToString returns correct string", "[choiceToString]") {
-    REQUIRE(choiceToString(1) == "Rock");
-    REQUIRE(choiceToString(2) == "Paper");
-    REQUIRE(choiceToString(3) == "Scissors");
-    REQUIRE(choiceToString(0) == "Invalid");
-    REQUIRE(choiceToString(99) == "Invalid");
-}

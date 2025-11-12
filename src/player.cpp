2ae6a21881c601323f9a4a05fea9e065e93f838e
yumao <maoyu2024@outlook.com>
2025-11-12 18:06:39 +0000

    implement function updateRecord

--- src/player.cpp
+++ src/player.cpp
@@ -1,3 +1,28 @@
 //
 // Created by maoyu on 2025/11/12.
 //
+
+#include "player.h"
+
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+// 根据胜负结果更新记录
+// result = 0 → 平局，1 → 玩家1赢，2 → 玩家2赢
+void updateRecord(Player& p1, Player& p2, int result) {
+    if (result == 0) {
+        p1.draws++;
+        p2.draws++;
+    } else if (result == 1) {
+        p1.wins++;
+        p2.losses++;
+    } else if (result == 2) {
+        p1.losses++;
+        p2.wins++;
+    }
+}
--- src/player.h
+++ src/player.h
@@ -5,4 +5,16 @@
 #ifndef ROCK_PAPER_SCISSORS_GAME_PLAYER_H
 #define ROCK_PAPER_SCISSORS_GAME_PLAYER_H
 
+#include <string>
+#include <map>
+
+
+struct Player {
+    std::string name;
+    int wins = 0;
+    int losses = 0;
+    int draws = 0;
+};
+void updateRecord(Player& p1, Player& p2, int result);
+
 #endif //ROCK_PAPER_SCISSORS_GAME_PLAYER_H
